27bb6bc0e8ddb6fbc77e9146f415ccca2468f484
Sistomin <sergej.istomin.2017@inbox.ru>
2022-04-17 21:49:08 +0700

    First iteration HW2

---
+++ src/main/java/ru/gb/istomin/homework2/ThirdTaskHW2.java
@@ -0,0 +1,15 @@
+package ru.gb.istomin.homework2;
+
+public class ThirdTaskHW2 {
+    public static void main(String[] args) {
+        trueOrFalse(-154);
+          }
+    public static boolean trueOrFalse(int a){
+        if (a < 0){
+            return true;
+        }
+        return false;
+    }
+}
+/*Написать метод, которому в качестве параметра передается целое число. Метод должен
+вернуть true, если число отрицательное, и вернуть false если положительное.*/
